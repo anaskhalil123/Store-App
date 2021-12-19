d3dd96704d42930ec3ead0f632b624aae0fb658a
Mac Pc <anas@Macs-Mac.local>
2021-12-19 11:59:40 -0800

    Initial Commit

---
+++ Store App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Store App
+//
+//  Created by Mac Pc on 12/19/21.
+//  Copyright © 2021 Mac Pc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
